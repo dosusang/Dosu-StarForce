ed4ccb88fed8d929beb2ed4bd7f88cdbd8c80ef7
maomao <maomao@DESKTOP-8ST2ID0>
2020-12-29 00:53:40 +0800

    eulerangle

---
+++ Assets/EulerRotationExam/LockRatation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class LockRatation : MonoBehaviour
+{
+    public bool lockX, lockY, lockZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var euler_angle = transform.rotation.eulerAngles;
+        if (lockX) euler_angle.x = 0;
+        if (lockY) euler_angle.y = 0;
+        if (lockZ) euler_angle.z = 0;
+        transform.localRotation= Quaternion.Euler(euler_angle);
+    }
+}
