66f1ca0186f5e42a69354a15b5c2b4df06f7145f
maomao <maomao@DESKTOP-8ST2ID0>
2021-01-07 15:18:14 +0800

    add dotween, test a shader

---
+++ Assets/GameMain/Scripts/Constom/GameMain.cs
@@ -0,0 +1,37 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Material mat;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            float getFloat()
+            {
+                return mat.GetFloat("_ColorRatio");
+            }
+            void setFloat(float x)
+            {
+                mat.SetFloat("_ColorRatio", x);
+            }
+            var tween = DOTween.To(getFloat, setFloat, 2, (float)0.5);
+            tween.onComplete = () =>
+            {
+                mat.SetFloat("_ColorRatio", 1);
+            };
+            tween.SetEase(Ease.OutBounce);
+        }
+    }
+}
