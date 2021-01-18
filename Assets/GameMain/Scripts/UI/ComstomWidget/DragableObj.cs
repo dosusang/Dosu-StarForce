381bb498e60e80d65ed144ba1148045df16b006f
dosusang <2501488984@qq.com>
2021-01-18 11:24:27 +0800

    重做拖拽组件

---
+++ Assets/GameMain/Scripts/UI/ComstomWidget/DragableObj.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace StarForce {
+    /// <summary>
+    /// 可拖动UI
+    /// </summary>
+    public class DragableObj : MonoBehaviour, IDragHandler {
+        private RectTransform rectTransform;
+
+
+        // Start is called before the first frame update
+        void Start() {
+            rectTransform = transform as RectTransform;
+        }
+        public void OnDrag(PointerEventData eventData) {
+            var globalMousePos = Vector3.zero;
+            if (RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, eventData.position, eventData.pressEventCamera, out globalMousePos)) {
+                rectTransform.position = globalMousePos;
+                rectTransform.rotation = rectTransform.rotation;
+            }
+        }
+    }
+
+}
